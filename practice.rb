39dc0628a397aca9168603b870a871dc9a411d07
Ray <rjjames92@gmail.com>
2015-05-01 15:21:57 +0100

    tried writting a method

--- practice.rb
+++ practice.rb
@@ -1,1 +1,11 @@
-name = jordan
+myname = "jordan"
+
+
+
+puts "whats your name?"
+gets.chomp
+
+
+def say_happy_birthday_to(name, repetitions)
+   repetitions.times { |i| puts "Happy Birthday #{name}"}
+end
